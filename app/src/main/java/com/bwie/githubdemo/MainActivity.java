f3086add3579c220526f2c07d2a7901a3f9ef1ab
songcongcong <31440037+songcongcong@users.noreply.github.com>
2017-09-27 09:49:56 +0800

    Update MainActivity.java
    测试

--- app/src/main/java/com/bwie/githubdemo/MainActivity.java
+++ app/src/main/java/com/bwie/githubdemo/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //第一次提交
+        
+        //第二个修改啦
     }
 }
